5a7a958f491b032b70524f90ede3702131113f0c
gianxz9 <161588605+gianxz9@users.noreply.github.com>
2024-04-30 08:42:44 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,19 +13,19 @@ function dividir() {
 
   //Captura o que está dentro do primeiro input no formulário
   //Number - Transforma o texto capturado em Numero
-  let primeiro_numero_inteiro = Number(document.getElementById("primeiro_numero_inteiro").value);
+  let distancia_percorrida_por_km = Number(document.getElementById("distancia_percorrida_por_km").value);
 
   //Captura o que está dentro do segundo input no formulário
   //Number - Transforma o texto capturado em Numero
-  let segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value);
+  let tempo_em_horas = Number(document.getElementById("tempo_em_horas").value);
 
   //Realiza a soma do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
   //variável "segundo_numero_inteiro"
   //E a variável "resultado_divisao" recebe o valor da soma
-  let resultado_divisao = primeiro_numero_inteiro / segundo_numero_inteiro;
+  let velocidade_media = distancia_percorrida_por_km / tempo_em_horas;
 
   //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_divisao"
-  document.getElementById("resultado_divisao").value = resultado_divisao;
+  document.getElementById("velocidade_media").value = velocidade_media;
 
 
 }
