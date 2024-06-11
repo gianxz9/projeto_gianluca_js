e10e356cdb2f311c3312bbf38be3bbc9b16a202a
gianxz9 <161588605+gianxz9@users.noreply.github.com>
2024-06-11 08:41:27 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,30 +2,45 @@ var meuFormulario = document.getElementById("formulario");
 meuFormulario.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  //Chama a função dividir
-  dividir();
-
-});
-
-
-//Função diviir
-function dividir() {
-
-  //Captura o que está dentro do primeiro input no formulário
-  //Number - Transforma o texto capturado em Numero
-  let distancia_percorrida_por_km = Number(document.getElementById("distancia_percorrida_por_km").value);
-
-  //Captura o que está dentro do segundo input no formulário
-  //Number - Transforma o texto capturado em Numero
-  let tempo_em_horas = Number(document.getElementById("tempo_em_horas").value);
-
-  //Realiza a soma do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
-  //variável "segundo_numero_inteiro"
-  //E a variável "resultado_divisao" recebe o valor da soma
-  let velocidade_media = distancia_percorrida_por_km / tempo_em_horas;
-
-  //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_divisao"
-  document.getElementById("velocidade_media").value = velocidade_media;
+  //Chama a função somar
+    somar();
+  }
 
 
-}
+  function somar() {
+
+    var valorlido
+    function lerletra() {
+        valorlido = document.getElementById("primeiro_numero_inteiro").value;
+        return console.log(valorlido);
+
+    function isvogal(caracter) {
+        switch (caracter) {
+            case "a":
+            case "A":
+            case "e":
+            case "E":
+            case "i":
+            case "I":
+            case "o":
+            case "O":
+            case "u":
+            case "U":
+            break
+            return true
+            default:
+            return false
+        }
+  }
+        var letra
+        lerletra ();
+        if (isvogal(letra) == true) {
+            alert("A letra é Vogal")
+        } else {
+            alert("A letra é consoante")
+        }
+
+
+
+
+  }
